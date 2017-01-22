e65630c70897d8891df09b2d8227b6e3890b3c75
Simon Birchall <sbirchall@Simons-MacBook-Pro.local>
2017-01-22 21:50:49 +0000

    Implement method_1, ruby_chop

--- app/chop.rb
+++ app/chop.rb
@@ -1,2 +1,7 @@
-def chop
+NOT_FOUND = -1
+def ruby_chop(target, arr)
+    return NOT_FOUND if arr.length == 0
+    index = arr.find_index(target)
+    return NOT_FOUND if index.nil?
+    index
 end
--- test/chop_test.rb
+++ test/chop_test.rb
@@ -3,30 +3,30 @@ require_relative "../app/chop"
 
 class TestChop < Minitest::Test
     def test_chop_method_exists
-        assert !!defined? chop
+        assert !!defined? ruby_chop
     end
 
-    def test_chop
-        assert_equal(-1, chop(3, []))
-        assert_equal(-1, chop(3, [1]))
-        assert_equal(0,  chop(1, [1]))
+    def test_ruby_chop
+        assert_equal(-1, ruby_chop(3, []))
+        assert_equal(-1, ruby_chop(3, [1]))
+        assert_equal(0,  ruby_chop(1, [1]))
         #
-        assert_equal(0,  chop(1, [1, 3, 5]))
-        assert_equal(1,  chop(3, [1, 3, 5]))
-        assert_equal(2,  chop(5, [1, 3, 5]))
-        assert_equal(-1, chop(0, [1, 3, 5]))
-        assert_equal(-1, chop(2, [1, 3, 5]))
-        assert_equal(-1, chop(4, [1, 3, 5]))
-        assert_equal(-1, chop(6, [1, 3, 5]))
+        assert_equal(0,  ruby_chop(1, [1, 3, 5]))
+        assert_equal(1,  ruby_chop(3, [1, 3, 5]))
+        assert_equal(2,  ruby_chop(5, [1, 3, 5]))
+        assert_equal(-1, ruby_chop(0, [1, 3, 5]))
+        assert_equal(-1, ruby_chop(2, [1, 3, 5]))
+        assert_equal(-1, ruby_chop(4, [1, 3, 5]))
+        assert_equal(-1, ruby_chop(6, [1, 3, 5]))
         #
-        assert_equal(0,  chop(1, [1, 3, 5, 7]))
-        assert_equal(1,  chop(3, [1, 3, 5, 7]))
-        assert_equal(2,  chop(5, [1, 3, 5, 7]))
-        assert_equal(3,  chop(7, [1, 3, 5, 7]))
-        assert_equal(-1, chop(0, [1, 3, 5, 7]))
-        assert_equal(-1, chop(2, [1, 3, 5, 7]))
-        assert_equal(-1, chop(4, [1, 3, 5, 7]))
-        assert_equal(-1, chop(6, [1, 3, 5, 7]))
-        assert_equal(-1, chop(8, [1, 3, 5, 7]))
+        assert_equal(0,  ruby_chop(1, [1, 3, 5, 7]))
+        assert_equal(1,  ruby_chop(3, [1, 3, 5, 7]))
+        assert_equal(2,  ruby_chop(5, [1, 3, 5, 7]))
+        assert_equal(3,  ruby_chop(7, [1, 3, 5, 7]))
+        assert_equal(-1, ruby_chop(0, [1, 3, 5, 7]))
+        assert_equal(-1, ruby_chop(2, [1, 3, 5, 7]))
+        assert_equal(-1, ruby_chop(4, [1, 3, 5, 7]))
+        assert_equal(-1, ruby_chop(6, [1, 3, 5, 7]))
+        assert_equal(-1, ruby_chop(8, [1, 3, 5, 7]))
     end
 end
