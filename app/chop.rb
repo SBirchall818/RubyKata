dc696c31b1859839225de1f9209af002e233f31a
Simon Birchall <sbirchall@Simons-MacBook-Pro.local>
2017-01-23 22:09:17 +0000

    Second chop kata

--- app/chop.rb
+++ app/chop.rb
@@ -5,3 +5,12 @@ def ruby_chop(target, arr)
     return NOT_FOUND if index.nil?
     index
 end
+
+def custom_chop(target, arr)
+    index = 0
+    for item in arr
+        return index if item === target
+        index += 1
+    end
+    NOT_FOUND
+end
--- test/chop_test.rb
+++ test/chop_test.rb
@@ -29,4 +29,32 @@ class TestChop < Minitest::Test
         assert_equal(-1, ruby_chop(6, [1, 3, 5, 7]))
         assert_equal(-1, ruby_chop(8, [1, 3, 5, 7]))
     end
+
+    def test_custom_chop_method_exists
+        assert !!defined? custom_chop
+    end
+
+    def test_custom_chop
+        assert_equal(-1, custom_chop(3, []))
+        assert_equal(-1, custom_chop(3, [1]))
+        assert_equal(0,  custom_chop(1, [1]))
+        #
+        assert_equal(0,  custom_chop(1, [1, 3, 5]))
+        assert_equal(1,  custom_chop(3, [1, 3, 5]))
+        assert_equal(2,  custom_chop(5, [1, 3, 5]))
+        assert_equal(-1, custom_chop(0, [1, 3, 5]))
+        assert_equal(-1, custom_chop(2, [1, 3, 5]))
+        assert_equal(-1, custom_chop(4, [1, 3, 5]))
+        assert_equal(-1, custom_chop(6, [1, 3, 5]))
+        #
+        assert_equal(0,  custom_chop(1, [1, 3, 5, 7]))
+        assert_equal(1,  custom_chop(3, [1, 3, 5, 7]))
+        assert_equal(2,  custom_chop(5, [1, 3, 5, 7]))
+        assert_equal(3,  custom_chop(7, [1, 3, 5, 7]))
+        assert_equal(-1, custom_chop(0, [1, 3, 5, 7]))
+        assert_equal(-1, custom_chop(2, [1, 3, 5, 7]))
+        assert_equal(-1, custom_chop(4, [1, 3, 5, 7]))
+        assert_equal(-1, custom_chop(6, [1, 3, 5, 7]))
+        assert_equal(-1, custom_chop(8, [1, 3, 5, 7]))
+    end
 end
